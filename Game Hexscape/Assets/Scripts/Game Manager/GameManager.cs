481bd119707440710851ff1385ffba344b0df514
KrisPearson <pearsonk1@hotmail.co.uk>
2018-12-12 23:09:59 +0000

    Error Fix
    > Push to fix errors in code from the last commit that was performed early due to outside circumstances.
    > Game Manager and state classes still not implemented, however they are almost ready to do so
    >State transition handling has been added, but not yet tested, using a generic container class

--- Game Hexscape/Assets/Scripts/Game Manager/GameManager.cs
+++ Game Hexscape/Assets/Scripts/Game Manager/GameManager.cs
@@ -56,15 +56,18 @@ public class GameManager : MonoBehaviour {
 
     public GameStateEndless endlessState;
 
-    class StateTransition
+    class StateTransition<TOne,TTwo> 
+        //where TOne : System.Type
+        where TTwo : struct, System.IConvertible // Intended to be an enum
     {
-        readonly GameStateBase CurrentState;
-        readonly Command Command;
 
-        public StateTransition(GameStateBase currentState, Command command)
+        readonly TOne keyOne;
+        readonly TTwo keyTwo;
+
+        public StateTransition(TOne keyOne, TTwo keyTwo)
         {
-            CurrentState = currentState;
-            Command = command;
+            this.keyOne = keyOne;
+            this.keyTwo = keyTwo;
         }
 
         public override int GetHashCode()
@@ -72,34 +75,39 @@ public class GameManager : MonoBehaviour {
             // Note on hash code overriding: https://www.baeldung.com/java-hashcode
             //return 17 + 31 * CurrentState.GetHashCode() + 31 * Command.GetHashCode();
             int returnHashCode = 17;
-            returnHashCode = returnHashCode * 23 + CurrentState.GetHashCode();
-            returnHashCode = returnHashCode * 23 + Command.GetHashCode();
+            returnHashCode = returnHashCode * 23 + keyOne.GetHashCode();
+            returnHashCode = returnHashCode * 23 + keyTwo.GetHashCode();
             return base.GetHashCode();
         }
 
         public override bool Equals(object obj)
         {
-            StateTransition other = obj as StateTransition;
-            return other != null && this.CurrentState == other.CurrentState && this.Command == other.Command;
+            StateTransition<TOne, TTwo> other = obj as StateTransition<TOne, TTwo>;
+            return other != null && Object.Equals(this.keyOne, other.keyOne) && Object.Equals(this.keyTwo, other.keyTwo); // TODO: ensure object.equals works in this context
+            //return other != null && this.CurrentState == other.CurrentState && this.Command == other.Command;
         }
     }
 
-
-    Dictionary<StateTransition, GameStateBase> transitions;
-
-    Dictionary<GameStateBase, GameStateBase> thing;
-
     private void InitialiseTransitions()
     {
+        Dictionary < StateTransition <System.Type, Command >, System.Type> transitions;
 
-        thing = new Dictionary<GameStateBase, GameStateBase>
-        {
-            {endlessState, typeof(GameStateEndless) }
-        };
+        //string stringTemp = endlessState.GetType().Name.ToString();
 
-        transitions = new Dictionary<StateTransition, GameStateBase>
+
+        //thing = new Dictionary<string, typeof(GameStateBase)>
+        //{
+        //    {  stringTemp, typeof(GameStateEndless),
+
+
+        //    }
+        //};
+
+        //thing.Add(stringTemp, typeof(GameStateEndless));
+
+        transitions = new Dictionary<StateTransition<System.Type, Command>, System.Type>
         {
-            { new StateTransition(GameStateEndless, Command.Begin), typeof(GameStateEndless) }
+            { new StateTransition<System.Type, Command> (typeof(GameStateEndless), Command.Begin), typeof(GameStateEndless) }
         };
     }
 
