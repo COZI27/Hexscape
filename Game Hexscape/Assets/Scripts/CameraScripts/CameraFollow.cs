3e65a0d2f4e0faf2e86bf6356229bc57258579fd
KrisPearson <pearsonk1@hotmail.co.uk>
2019-04-21 10:30:51 +0100

    Revert "Merge remote-tracking branch 'origin/levelEditor' into Tunnel-Revisted"
    This reverts commit f8cfb8e13cc3680be2cde10058146027ef6f7a52, reversing
    changes made to f4d4e4775941bc2a6e64e542f70627660e265c3c.

--- Game Hexscape/Assets/Scripts/CameraScripts/CameraFollow.cs
+++ Game Hexscape/Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -14,72 +14,26 @@ public class CameraFollow : MonoBehaviour {
 
     public float smoothSpeed;
 
-    float mapFitOffset;
-
     [ContextMenu("SetOffset")]
     public void SetOffset ()
     {
         offset = transform.position - target.position;
     }
 
-
-   
-
-    void Update()
-    {
-        Bounds mapBounds = new Bounds();
-        mapBounds.size = Vector3.zero; // reset
-        Hex[] HexObjects = MapSpawner.instance.grid.GetComponentsInChildren<Hex>();
-        // Debug.Log(HexObjects.Length);
-        foreach (Hex hex in HexObjects)
-        {
-            mapBounds.Encapsulate(hex.GetComponent<Collider>().transform.position);
-        }
-
-        float aspectRatio = (float) Screen.width / (float) Screen.height;
-        Debug.Log(aspectRatio);
-        float tanFov = Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2.0f);
-
-         mapFitOffset = (mapBounds.extents.x / 2.0f / aspectRatio / tanFov);
-        Debug.Log(mapFitOffset);
-        
-    }
-
     private void LateUpdate()
     {
-        if (target == null)
+       if (target == null)
         {
             return;
         }
 
-        Bounds mapBounds = new Bounds();
-
-
-
-        mapBounds.size = Vector3.zero; // reset
-        Hex[] HexObjects = MapSpawner.instance.grid.GetComponentsInChildren<Hex>();
-        // Debug.Log(HexObjects.Length);
-        foreach (Hex hex in HexObjects)
-        {
-
-            mapBounds.Encapsulate(hex.GetComponent<Collider>().transform.position);
-        }
-
-
-
-
-
         if (smoothY)
         {
-            Vector3 endPos = target.position + offset + Vector3.up* 2 * mapFitOffset;
-
+            Vector3 endPos = target.position + offset;
             Vector3 currentPos = Vector3.Lerp(transform.position, endPos, smoothSpeed * Time.deltaTime);
 
-
-
             transform.position = currentPos;
-        }
-        else
+        } else
         {
             Vector3 endPos = target.position + offset;
             Vector3 currentPos = Vector3.Lerp(transform.position, endPos, smoothSpeed * Time.deltaTime);
@@ -87,7 +41,7 @@ public class CameraFollow : MonoBehaviour {
 
             transform.position = currentPos;
         }
-
+       
 
     }
 
